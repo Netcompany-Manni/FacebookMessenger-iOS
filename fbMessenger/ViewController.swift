f65cb6ace3ab979dae107c64c0c9c23037e40f56
Manveer Singh Pandher <manvep@netcompany.com>
2018-12-20 12:55:18 +0100

    Finished collectionView setup and layouts for Recent messages

--- fbMessenger/ViewController.swift
+++ fbMessenger/ViewController.swift
@@ -65,20 +65,73 @@ class FriendCell : BaseCell{
         return view
     }()
     
+    let nameLabel : UILabel = {
+        let label : UILabel = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Friend Name"
+        return label
+    }()
+    
+    let messageLabel : UILabel = {
+        let messageLabel : UILabel = UILabel()
+        messageLabel.font = UIFont.systemFont(ofSize: 14)
+        messageLabel.text = "Friends Message and something fadeeeeeee"
+     
+        messageLabel.backgroundColor = UIColor.darkGray
+        return messageLabel
+    }()
+    
+    let timeLabel : UILabel = {
+        let timeLabel : UILabel = UILabel()
+        timeLabel.font = UIFont.systemFont(ofSize: 16)
+        timeLabel.text = "12:05 pm"
+        timeLabel.backgroundColor = UIColor.yellow
+        timeLabel.textAlignment = .right
+        return timeLabel
+    }()
+    
     override func setupViews() {
-        backgroundColor = UIColor.white
+        backgroundColor = UIColor.gray
+        
         addSubview(dividerLineView)
         addSubview(profileImageView)
+        
+        setupContainerView() //Will add a containerview as a subView. In the function
+        
         dividerLineView.translatesAutoresizingMaskIntoConstraints = false
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
         profileImageView.image = #imageLiteral(resourceName: "newHarambe")
         
-        //Constraints for the divider line at the bottom of the cell
-        addConstraintsWithFormat(format: "H:|-82-[v0]|", views: dividerLineView)
-        addConstraintsWithFormat(format: "V:[v0(1)]|", views: dividerLineView)
         //Constraints for the image in the cell
         addConstraintsWithFormat(format: "H:|-12-[v0(68)]", views: profileImageView)
-        addConstraintsWithFormat(format: "V:|-12-[v0(68)]", views: profileImageView)
+        addConstraintsWithFormat(format: "V:[v0(68)]", views: profileImageView)
+        //Constraints the height to be the center of the cell
+        addConstraint(NSLayoutConstraint(item: profileImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        //Constraints for the divider line at the bottom of the cell
+        addConstraintsWithFormat(format: "H:|-82-[v0]|", views: dividerLineView)
+        addConstraintsWithFormat(format: "V:[v0(1)]", views: dividerLineView)
+    
+    }
+    
+    private func setupContainerView(){
+        let containerView = UIView()
+        containerView.backgroundColor = UIColor.blue
+       
+        addSubview(containerView)
+        
+        addConstraintsWithFormat(format: "H:|-90-[v0]|", views: containerView)
+        addConstraintsWithFormat(format: "V:[v0(50)]", views: containerView)
+         //Constraint to the height to be the center of the cell
+        addConstraint(NSLayoutConstraint(item: (containerView), attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addConstraintsWithFormat(format: "H:|[v0][v1(80)]-12-|", views: nameLabel, timeLabel) //NameLabel and timeLabel
+        
+        containerView.addConstraintsWithFormat(format: "H:|[v0]-12-|", views: messageLabel) //MsgLabel
+        containerView.addConstraintsWithFormat(format: "V:|[v0][v1(24)]|", views: nameLabel, messageLabel)
+        containerView.addConstraintsWithFormat(format: "V:|[v0(20)]", views: timeLabel)
     }
 }
 
@@ -95,6 +148,7 @@ extension UIView {
             viewsDictionary[key] = view
             view.translatesAutoresizingMaskIntoConstraints = false
         }
+       //Adds constraints on all the incomming views
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
    
     }
